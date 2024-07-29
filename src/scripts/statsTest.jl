51b31f7e7be4ad570b878023b04e4d61f56f22c8
NatMath <64751772+NatMath@users.noreply.github.com>
2024-07-29 10:10:40 +0200

    Initial Fitting
    Added StableDistributions.jl
    Initial fitting tests
    Not great fits still, need to play with parameters

--- src/scripts/statsTest.jl
+++ src/scripts/statsTest.jl
@@ -1,3 +1,5 @@
+using StableDistributions
+
 # Sample from plaw distribution, test binning/plotting
 function plawSampling()
     N = 100000 #Number of samples to take
@@ -13,26 +15,42 @@ end
 export plawSampling
 
 function sumOfSamples()
-    m = 10000
-    N = 10000
+    m = 100_00
+    N = 100_00
     α=1.4
     shape=α-1
     dist = Pareto(shape)
     samples = [sum(rand(dist, N)) for _ in 1:m]
+    return (m, N, samples)
+end
+export sumOfSamples
 
+function sumOfSamplesAnalysis(m, N, samples)
     Nbins = round(Int, N/100)
     bins, counts = trimZeros(logBinning(samples, Nbins, min = 1)...)
-    scatter(bins, counts, xaxis=:log, yaxis=:log, label="Sum of plaw")
+    p = scatter(bins, counts, xaxis=:log, yaxis=:log, label="Sum of plaw")
 
     μ, σ = 2e7, 1e9
     scale = 1e6
-    levySamples = rand(Levy(μ, σ), 100000)
-    plot!(trimZeros(logBinning(levySamples, 1000, min=1)...)...,xaxis=:log, yaxis=:log, label = "Sampled Lévy")
+    #levySamples = rand(Levy(μ, σ), 100000)
+    #plot!(trimZeros(logBinning(levySamples, 1000, min=1)...)...,xaxis=:log, yaxis=:log, label = "Sampled Lévy")
     #xs = LinRange(minimum(samples), maximum(samples), 1000)
     #ys = @. scale*sqrt(σ/(2π*(xs-μ)^3))*exp(-σ/(2*(xs-μ)))
     #plot!(xs, scale * ys, label = "Lévy PDF")
+
+    levyN = round(Int, 2.5m)
+    fitDist = StableDistributions.fit(Stable, samples)
+    print(fitDist)
+    fitSamples = rand(fitDist, levyN)
+    plot!(p, trimZeros(logBinning(fitSamples, Nbins, min=1)...)...,xaxis=:log, yaxis=:log, label = "Sampled Lévy")
+
+    for c in [1e10, 5e10, 1e11, 3e11, 7e11, 1e12, 5e12, 1e13, 5e13, 1e14]#0:0.1:1.
+        dist = Stable(fitDist.α, fitDist.β, c, fitDist.μ)
+        plot!(p, trimZeros(logBinning(rand(dist, levyN), Nbins, min=1)...)...,xaxis=:log, yaxis=:log, label = c)
+    end
+    p
 end
-export sumOfSamples
+export sumOfSamplesAnalysis
 
 function plotPDFs()
     xs = 10 .^ LinRange(0, 4, 100)
