45c372c126c8f2186857be7fe05f182100822e8f
NatMath <64751772+NatMath@users.noreply.github.com>
2024-08-02 12:03:14 +0200

    Initial clustering algorithm
    Simple cluster identification, counting and distribution
    2D
    normal boundary conditions

--- src/tools/gridTools.jl
+++ src/tools/gridTools.jl
@@ -43,3 +43,65 @@ function spatialCorrelation(grid, cutoffDist = size(grid, 1) - 1)
     return (distances[counts .!= 0], autocorrs[counts .!= 0])
 end
 export spatialCorrelation
+
+function findClusters2D(grid::Matrix{Bool})
+    # Based on the naive Hoshen-Kopelman algorithm
+    #https://www.ocf.berkeley.edu/~fricke/projects/hoshenkopelman/hoshenkopelman.html
+
+    # Expects a grid of booleans values, with true being occupied
+    # Doesn't need to be square
+
+    L = size(grid, 1)
+    N = prod(size(grid))
+
+    labels = collect(1:N)
+
+    for i = 1:size(grid, 2), j = 1:size(grid, 1)
+        idx = (i-1)*L+j
+        grid[j,i] || continue #If unoccupied, continue to next grid cell
+        left = (i > 1 && grid[j, i-1]) 
+        up = (j > 1 && grid[j-1, i])   
+
+        !left && !up && continue
+        if left && ! up
+            labels[idx] = labels[idx-L]
+        elseif !left && up
+            labels[idx] = labels[idx-1]
+        elseif left && up
+            union!(labels, idx-L, idx-1)
+            labels[idx] = labels[idx-L]
+        end
+    end
+    return [find!(labels, x) for x in 1:N]
+end
+export findClusters2D
+# Helper methods. Not exported
+function union!(labels, x, y)
+    labels[find!(labels, x)] = find!(labels, y)
+    return labels
+end
+function find!(labels, x)
+    y = x
+    while labels[y] != y #Find the root
+        y = labels[y]
+    end 
+    while labels[x] != x #Set all the nodes on the way directly to the root
+        z = labels[x]
+        labels[x] = y
+        x = z
+    end
+    return y
+end
+
+function countClusters2D(grid::Matrix{Bool})
+    clusters = reshape(findClusters2D(grid), size(grid)) .* grid
+    return length(Set(clusters)) - 1 #Remove 0, which corresponds to the points where the grid is unoccupied
+end
+export countClusters2D
+
+function clusterSizes2D(grid::Matrix{Bool})
+    clusters = reshape(findClusters2D(grid), size(grid)) .* grid
+    sizes = [count(==(n), clusters) for n in unique(clusters[clusters .!= 0])]
+    return sizes
+end
+export clusterSizes2D
